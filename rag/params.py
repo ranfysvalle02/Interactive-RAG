42aa00248d6c4b783b466832b1a29a6c161b8785
benjibc <youfychenbc5000@gmail.com>
2024-01-20 16:53:11 -0800

    Adding Fireworks integration with Mongo Interactive RAG

--- rag/params.py
+++ rag/params.py
@@ -1,16 +1,25 @@
 # For Google Search
 SERPAPI_KEY = ""
 
-# MongoDB 
+# MongoDB
+
+
 MONGODB_URI = ""
 DATABASE_NAME = "genai"
 COLLECTION_NAME = "rag"
 
 # If using OpenAI
 OPENAI_API_KEY = ""
+OPENAI_BASE_URL = "https://api.openai.com/v1"
+MODEL_NAME = "gpt-3.5-turbo"
+
+# If using Fireworks
+# OPENAI_API_KEY = "FIREWORKS_API_KEY"
+# OPENAI_BASE_URL = "https://api.fireworks.ai/inference/v1"
+# MODEL_NAME = "accounts/fireworks/models/firefunction-v1"
 
 # If using Azure for OpenAI
-OPENAI_TYPE = "azure"
+OPENAI_TYPE = ""
 OPENAI_API_VERSION = "2023-10-01-preview"
 OPENAI_AZURE_ENDPOINT = "https://.openai.azure.com/"
 OPENAI_AZURE_DEPLOYMENT = ""
